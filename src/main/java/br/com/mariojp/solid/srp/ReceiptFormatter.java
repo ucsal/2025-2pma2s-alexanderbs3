a30593d72140648b5cd39ce4a204569485614473
alexanderbs3 <alexander.cbss@hotmail.com>
2025-08-26 14:30:48 -0300

    Refatoração SRP - Implementação do Princípio da Responsabilidade Única

---
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptFormatter.java
@@ -0,0 +1,25 @@
+package br.com.mariojp.solid.srp;
+
+import java.util.List;
+
+public class ReceiptFormatter {
+
+    public String format(List<Item> items, double subtotal, double tax, double total) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("=== RECIBO ===\n");
+
+        for (Item item : items) {
+            double itemTotal = item.getUnitPrice() * item.getQuantity();
+            sb.append(item.getName())
+                    .append(" x")
+                    .append(item.getQuantity())
+                    .append(" = ")
+                    .append(itemTotal)
+                    .append("\n");
+        }
+        sb.append("Subtotal: ").append(subtotal).append("\n");
+        sb.append("Tax: ").append(tax).append("\n");
+        sb.append("Total: ").append(total).append("\n");
+        return sb.toString();
+    }
+}
--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,19 +1,34 @@
 package br.com.mariojp.solid.srp;
 
 public class ReceiptService {
-	public String generate(Order order) {
-		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
-		double total = subtotal + tax;
-		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
-		sb.append("=== RECIBO ===\n");
-		for (var i : order.getItems()) {
-			sb.append(i.getName()).append(" x").append(i.getQuantity()).append(" = ").append(i.getUnitPrice() * i.getQuantity())
-					.append("\n");
-		}
-		sb.append("Subtotal: ").append(subtotal).append("\n");
-		sb.append("Tax: ").append(tax).append("\n");
-		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
-	}
+
+    private final TaxCalculator taxCalculator;
+    private final ReceiptFormatter receiptFormatter;
+
+    public ReceiptService() {
+        this.taxCalculator = new TaxCalculator();
+        this.receiptFormatter = new ReceiptFormatter();
+    }
+
+    public ReceiptService(TaxCalculator taxCalculator, ReceiptFormatter receiptFormatter) {
+        this.taxCalculator = taxCalculator;
+        this.receiptFormatter = receiptFormatter;
+    }
+
+
+    public String generate(Order order) {
+        double subtotal = calculateSubtotal(order);
+
+        double tax = taxCalculator.calculateTax(subtotal);
+
+        double total = subtotal + tax;
+
+        return receiptFormatter.format(order.getItems(), subtotal, tax, total);
+    }
+
+    private double calculateSubtotal(Order order) {
+        return order.getItems().stream()
+                .mapToDouble(item -> item.getUnitPrice() * item.getQuantity())
+                .sum();
+    }
 }
